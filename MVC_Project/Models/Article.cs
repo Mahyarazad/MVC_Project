5e345070119d7a669f97d30004d39307202b9886
Mahyarazad <mhyrinc@gmail.com>
2021-10-08 15:47:31 +0330

    Views Updated & Partial View temporary added

---
+++ MVC_Project/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Project.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+    }
+}
---
+++ MVC_Project/Models/MotherClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MVC_Project.Models
+{
+    public class MotherClass
+    {
+        public List<PortFolio> PortFolio { get; set; }
+        public List<Article> Article { get; set; }
+    }
+}
