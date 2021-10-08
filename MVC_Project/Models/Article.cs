052a937103897d8379e60413c552f06d16d0889f
Mahyarazad <mhyrinc@gmail.com>
2021-10-08 16:31:28 +0330

    ViewComponents Added

--- MVC_Project/Models/Article.cs
+++ MVC_Project/Models/Article.cs
@@ -1,10 +1,23 @@
-﻿namespace MVC_Project.Models
+﻿using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Models
 {
+
     public class Article
     {
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImageName { get; set; }
+
+        public Article(long id, string title, string description, string imageName)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            ImageName = imageName;
+        }
     }
 }
---
+++ MVC_Project/ViewComponents/ArticleViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Models;
+
+namespace MVC_Project.ViewComponent
+{
+    public class ArticleViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1, "Snapp Taxi", "Public transportation service", "project-1.jpg"),
+                new Article(2, "Snapp Taxi", "Public transportation service", "project-2.jpg"),
+                new Article(3, "Snapp Taxi", "Public transportation service", "project-3.jpg"),
+                new Article(4, "Snapp Taxi", "Public transportation service", "project-4.jpg"),
+            };
+            return View("_Article", articles);
+        }
+
+    }
+}
